4d6c42cc566b95f2fb23e372a4a1e223fcc1cc5a
Jacob Stephens <jacobstephens2@gmail.com>
2021-12-19 20:56:25 -0800

    made a decent amount of progress, added carousel as well as acordion items

--- src/App.js
+++ src/App.js
@@ -1,10 +1,67 @@
-import NavBar from './components/NavBar'
+import React, { useState } from 'react'
+import companyLogo from './content/LearnToLiveCo-Operative.jpg'
+import { Accordion, Typography, AccordionSummary, AccordionDetails, ThemeProvider } from '@mui/material';
+import './styles.css'
+import Carousel from 'react-elastic-carousel'
+import Item from './components/Item'
+import z1 from './content/zoie1.JPG'
 
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1200, itemsToShow: 4 }
+];
 
 const App = () => {
+
+
+  const [items, setItems] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+
+
   return (
-    <div>
-      <NavBar/>
+  
+    <div className='layout'>
+
+      {/* <img className='logo'src={companyLogo} alt='LearnToLiveCo-OperativeLogo'/> */}
+
+        <div className='carousel'>
+        <Carousel itemsToShow={1}>
+          {/* {items.map((item) => (
+            <Item key={item}>{item}</Item>
+          ))} */}
+          <img src={companyLogo}/>
+          <img src={companyLogo}/>
+          <img src={companyLogo}/>
+          <img src={companyLogo}/>
+          {/* <Item><img src={z1}/></Item>
+          <Item><img src={companyLogo}/></Item>
+          <Item><img src={companyLogo}/></Item>
+          <Item><img src={companyLogo}/></Item> */}
+        </Carousel>
+        </div>
+
+
+
+        <Accordion className='accordion' square='true'>
+          <AccordionSummary className='accordionSummary'> 
+            <Typography className='accordionTitle'>About Us</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>Details</Typography>
+          </AccordionDetails>
+        </Accordion>
+
+
+        <Accordion className='accordion' square='true'>
+          <AccordionSummary>
+            <Typography>Contact</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>Contact Info</Typography>
+          </AccordionDetails>
+        </Accordion>
+
     </div>
   );
 }
---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export default styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    width:100%;
+    max-width: 100%;
+    background-color: #7FC880;
+    color: #fff;
+    margin: 15px;
+    font-size: 4em;
+    max-width: 100%;
+`;
