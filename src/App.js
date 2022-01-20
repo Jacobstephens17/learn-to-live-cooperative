473b4c166e8ec7d9cfd794e7da068aa1ae712d6a
Jacob Stephens <jacobstephens2@gmail.com>
2022-01-19 22:01:47 -0800

    updated some styling, got new images and added a container for the accordion to better manage styling

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './styles.css'
 import Carousel from 'react-elastic-carousel'
 import Item from './components/Item'
 import z1 from './content/zoie1.JPG'
+import Z from './content/Z.jpg'
 import food from './content/food.JPG'
 import plant from './content/plant.JPG'
 import plant2 from './content/plant2.JPG'
@@ -12,9 +13,9 @@ import drive from './content/drive.JPG'
 import drive2 from './content/drive2.JPG'
 import food2 from './content/food2.jpg'
 import hospital from './content/hospital.JPEG'
-import mimi from './content/mimi.JPG'
+import mimi from './content/mimi.jpg'
 import directors from './content/boardOfDirectors.JPG'
-
+import jaleadeanne from './content/jaleadeanne.jpg'
 
 const App = () => {
 
@@ -24,21 +25,22 @@ const App = () => {
     <div className='layout'>
 
 
-      <h1 className="name">Grow With Us</h1>
+      <h1 className="header">Grow With Us</h1>
 
         <Carousel enableSwipe="true" className="carousel" tiltEasing="ease" itemsToShow={1}>
-          <img className='images' src={companyLogo}/>
-          <img className='images' src={z1}/>
-          <img className='images' src={food}/>
-          <img className='images' src={plant}/>
-          <img className='images' src={plant2}/>
-          <img className='images' src={drive}/>
-          <img className='images' src={drive2}/>
-          <img className='images' src={hospital}/>
-          <img className='images' src={food2}/>
+          <img className='images' alt='logo' src={companyLogo}/>
+          <img className='images' alt='zoie' src={z1}/>
+          <img className='images' alt='food' src={food}/>
+          <img className='images' alt='plant' src={plant}/>
+          <img className='images' alt='plant two' src={plant2}/>
+          <img className='images' alt='food drive'src={drive}/>
+          <img className='images' alt='food drive 2'src={drive2}/>
+          <img className='images' alt='hospital' src={hospital}/>
+          <img className='images' alt='food' src={food2}/>
         </Carousel>
 
 
+        <div className='accordionContainer'>
 
         <Accordion className='accordion' square='true'>
 
@@ -88,14 +90,15 @@ const App = () => {
 
 
           <AccordionDetails align='center' className='accordionDetails'>
-            <img className='images' src={directors} />
+
+            <img className='images' alt='directors' src={directors} />
             <div className='seperator'></div>
             <Typography className='names'><span className="underlineForNames">Director of General Operations</span><br/>Zoie Laviolette</Typography>
-            <img className='images' src={z1}/>
+            <img className='images' alt='zoie' src={Z}/>
             <Typography className='names'><span className="underlineForNames">Director of Youth Outreach</span><br/>Noelani White</Typography>
-            <img className='images' src={companyLogo}/>
+            <img className='images' alt='company logo' src={companyLogo}/>
             <Typography className='names'> <span className="underlineForNames">Director of Arts</span><br/>Dominique Keys</Typography>
-            <img className='images' src={mimi}/>
+            <img className='images' alt='mimi' src={mimi}/>
           </AccordionDetails>
         </Accordion>
 
@@ -109,13 +112,16 @@ const App = () => {
 
           <AccordionDetails align='center' className='accordionDetails'>
             <Typography className='names'><span className='underlineForNames'>President</span><br/>Zoie Laviolette</Typography>
-            <img className='images' src={z1}/>
+            <img className='images' alt='zoie' src={Z}/>
             <Typography className='names'><span className='underlineForNames'>Secretary</span><br/>Chelsea Sconce</Typography>
-            <img className='images' src={companyLogo}/>
+            <img className='images' alt='company logo' src={companyLogo}/>
             <Typography className='names'><span className='underlineForNames'>Treasurer</span><br/>Jaleadeanne Traynor</Typography>
-            <img className='images' src={companyLogo}/>
+            <img className='images' alt='company logo' src={jaleadeanne}/>
           </AccordionDetails>
         </Accordion>
+
+
+        </div>
 
     </div>
   );
